c0b9a8d9fbbf91fcc3c57f5a7704941557f8af08
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-04-23 19:11:49 +0300

    jsx edit 2

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import { image } from "../data/data";
 function About() {
   return ( <div id="about" >
     <h2>About Me</h2>
-    <p>I'm Sese</p>
+    <p>A cool little phrase to decribe who I am ;) </p>
     <img src={image} alt ="I made this"/>
   </div>
   )
